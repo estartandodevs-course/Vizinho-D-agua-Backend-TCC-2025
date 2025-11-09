e002abfc16e0ebac9207102a5bba83c93fb0be34
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-09 00:13:32 -0300

    feat: add [DatabaseGenerated(DatabaseGeneratedOption.Computed)] in updatedAt

--- src/VizinhoDAgua.Domain/Entities/Entity.cs
+++ src/VizinhoDAgua.Domain/Entities/Entity.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VizinhoDAgua.Domain.Entities
 {
     public abstract class Entity
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime? UpdatedAt { get; set; }
     }   
 }
