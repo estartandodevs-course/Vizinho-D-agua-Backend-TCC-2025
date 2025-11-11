a55be8cc5084c76110c670f074913ef07e4c01e0
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-11 19:39:56 -0300

    feat: add update command and handler from community

---
+++ src/VizinhoDAgua.Application/UseCases/Communities/Command/Update/UpdateCommunityCommand.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using System.Net;
+using VizinhoDAgua.Application.Mediator;
+using VizinhoDAgua.Application.UseCases.Communities.Command.Create;
+
+namespace VizinhoDAgua.Application.UseCases.Communities.Command.Update
+{
+    public class UpdateCommunityCommand(string? title, string? description, string? coverImage) : IRequest<CommandResponse<Unit>>
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; } = title;
+        public string? Description { get; set; } = description;
+        public string? CoverImage { get; set; } = coverImage;
+
+        public ValidationResult validationResult { get; private set; }
+
+        public bool Validate()
+        {
+            var validations = new InlineValidator<CreateCommunityCommand>();
+
+            validations.RuleFor(c => c.Title)
+                .NotEmpty()
+                .WithErrorCode(((int)HttpStatusCode.BadRequest).ToString())
+                .WithMessage("O título é obrigatório.");
+
+            validations.RuleFor(c => c.Description)
+                .NotEmpty()
+                .WithErrorCode(((int)HttpStatusCode.BadRequest).ToString())
+                .WithMessage("A descrição é obrigatória.");
+
+            return validationResult.IsValid;
+        }
+    }
+}
---
+++ src/VizinhoDAgua.Application/UseCases/Communities/Command/Update/UpdateCommunityCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System.Net;
+using VizinhoDAgua.Application.Mediator;
+using VizinhoDAgua.Domain.Repositories;
+
+namespace VizinhoDAgua.Application.UseCases.Communities.Command.Update
+{
+    public class UpdateCommunityCommandHandler
+     (
+         ICommunityRepository communityRepository
+     ) : IRequestHandler<UpdateCommunityCommand, CommandResponse<Unit>>
+    {
+        private readonly ICommunityRepository _communityRepository = communityRepository;
+
+        public async Task<CommandResponse<Unit>> Handle(UpdateCommunityCommand request, CancellationToken cancellationToken)
+        {
+            if (!request.Validate())
+                return CommandResponse<Unit>.ErrorValidation(request.validationResult);
+
+            var community = await _communityRepository.GetByIdAsync(request.Id);
+            if (community == null)
+                return CommandResponse<Unit>.AddError(message: "Comunidade não encontrada.", statusCode: HttpStatusCode.NotFound);
+
+            await _communityRepository.UpdateAsync(community);
+
+            return CommandResponse<Unit>.Success(null);
+            
+        }
+    }
+}
