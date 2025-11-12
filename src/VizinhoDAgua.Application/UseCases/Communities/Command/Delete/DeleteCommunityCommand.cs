6aec6b6cb25d3dbd0d2335b5777f293ef799a9a9
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-11 22:54:12 -0300

    feat: add deleted command

---
+++ src/VizinhoDAgua.Application/UseCases/Communities/Command/Delete/DeleteCommunityCommand.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using VizinhoDAgua.Application.Mediator;
+
+namespace VizinhoDAgua.Application.UseCases.Communities.Command.Delete
+{
+    public class DeleteCommunityCommand : IRequest<CommandResponse<Unit>>
+    {
+        public Guid Id { get; private set; }
+        public ValidationResult validationResult { get; private set; }
+
+        public DeleteCommunityCommand(Guid id)
+        {
+            Id = id;
+        }
+
+        public bool Validate()
+        {
+            var validations = new InlineValidator<DeleteCommunityCommand>();
+
+            validations.RuleFor(c => c.Id)
+            .NotEmpty()
+            .WithMessage("O ID da comunidade é obrigatório para a atualização.");
+
+            validationResult = validations.Validate(this);
+
+            return validationResult.IsValid;
+        }
+    }
+}
