b8829628967ae6a9e81f480a07c003c481afe13a
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-12 16:55:08 -0300

    refactor: adjustments to the User entity
    - Adicionados setters privados e inicialização com string.Empty
    - Adicionado método Update(name, profileImage)

--- src/VizinhoDAgua.Domain/Entities/User.cs
+++ src/VizinhoDAgua.Domain/Entities/User.cs
@@ -2,28 +2,32 @@ namespace VizinhoDAgua.Domain.Entities
 {
     public class User : Entity
     { 
-        public string? Name { get; private set; }
-        public string? Email { get; private set; }
-        public string? Password { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string Password { get; private set; } = string.Empty;
         public bool IsAdmin { get; private set; }
-        public string? ProfileImage { get; private set; }
+        public string ProfileImage { get; private set; } = string.Empty;
         
         public List<Community> Communities { get; private set; } = [];
         public List<CommunityPost> Posts { get; private set; } = [];
         public List<Report> Reports { get; private set; } = [];
         
         public User() {  } // EF Core
-        
-        // inicializar os atributos com strings vazia "= string.Empty;" ~> padrão em projetos com EF Core sem required
-        // tornar atributos nulos "?" ~>
 
-        public User(string? name, string? email, string? password, string? profileImage)
+        public User(string name, string email, string password, string? profileImage)
         {
             Name = name;
             Email = email;
             Password = password;
             IsAdmin = false;
-            ProfileImage = profileImage;
+            ProfileImage = profileImage ?? string.Empty;
+        }
+        
+        public void Update(string name, string? profileImage)
+        {
+            Name = name;
+            ProfileImage = profileImage ?? string.Empty;
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
