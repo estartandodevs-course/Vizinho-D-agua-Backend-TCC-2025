d39f32089cddf3a1cd248453e12b4c02704d3d9e
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-21 02:18:59 -0300

    fix: update CreateEducationContentResponse to return only id
    Correçao do erro "Missing type map configuration" durante a criação

--- src/VizinhoDAgua.Application/UseCases/EducationContent/Commands/Create/CreateEducationContentResponse.cs
+++ src/VizinhoDAgua.Application/UseCases/EducationContent/Commands/Create/CreateEducationContentResponse.cs
@@ -1,23 +1,12 @@
-using VizinhoDAgua.Domain.Entities.Enum;
-
 namespace VizinhoDAgua.Application.UseCases.EducationContent.Commands.Create
 {
     public class CreateEducationContentResponse
     {
-        public string Title { get; private set; }
-        public string? Image { get; private set; }
-        public string Author { get; private set; }
-        public EducationContentType ContentType { get; private set; }
-        public string? FilePath { get; private set; }
+        public Guid Id { get; private set; }
 
-        public CreateEducationContentResponse(string title, string? image, string author, 
-            EducationContentType contentType, string? filePath)
+        public CreateEducationContentResponse(Guid id)
         {
-            Title = title;
-            Image = image;
-            Author = author;
-            ContentType = contentType;
-            FilePath = filePath;
+            Id = id;
         }
     }
 }
