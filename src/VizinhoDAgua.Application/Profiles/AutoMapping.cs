b2c60344b457a3bbde8a9381f78e203dd5a858a3
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-24 17:00:20 -0300

    feat: update DTOs and mapping profile for new command structures
    Atualiza os DTOs de criação/atualização e adiciona novos mapeamentos no AutoMapper, incluindo construção de comandos e prevenção de overwrite.

--- src/VizinhoDAgua.Application/Profiles/AutoMapping.cs
+++ src/VizinhoDAgua.Application/Profiles/AutoMapping.cs
@@ -4,6 +4,8 @@ using VizinhoDAgua.Application.UseCases.Community.Command.Create;
 using VizinhoDAgua.Application.UseCases.Community.Command.Update;
 using VizinhoDAgua.Application.UseCases.EducationContent.Commands.Create;
 using VizinhoDAgua.Application.UseCases.EducationContent.Commands.Update;
+using VizinhoDAgua.Application.UseCases.User.Commands.Create;
+using VizinhoDAgua.Application.UseCases.User.Commands.Update;
 using VizinhoDAgua.Domain.Entities;
 
 namespace VizinhoDAgua.Application.Profiles
@@ -42,6 +44,26 @@ namespace VizinhoDAgua.Application.Profiles
                     source.Request.FilePath
                 ));
             CreateMap<UpdateEducationContentCommand, EducationContentEntity>()
+                // evita sobrescrever propriedades quando o campo vem null
+                .ForAllMembers(opts 
+                    => opts.Condition((_, _, srcMember) => srcMember != null));
+        
+            // USER
+            // Request ~> Command
+            CreateMap<CreateUserRequest, CreateUserCommand>()
+                .ConstructUsing(source => new CreateUserCommand());
+            // Command ~> Entity
+            CreateMap<CreateUserCommand, UserEntity>();
+            // Update
+            CreateMap<(Guid Id, UpdateUserRequest Request), UpdateUserCommand>()
+                .ConstructUsing(source => new UpdateUserCommand(
+                    source.Id,
+                    source.Request.Name,
+                    source.Request.Email,
+                    source.Request.Password,
+                    source.Request.ProfileImage
+                ));
+            CreateMap<UpdateUserCommand, UserEntity>()
                 // evita sobrescrever propriedades quando o campo vem null
                 .ForAllMembers(opts 
                     => opts.Condition((_, _, srcMember) => srcMember != null));
