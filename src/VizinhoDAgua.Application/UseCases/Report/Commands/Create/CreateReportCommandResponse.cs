f13869c8f21830e0a923b1ad3c4de98e2da46939
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-25 12:07:37 -0300

    refactor: reorganized the postal service and project dependencies

--- src/VizinhoDAgua.Application/UseCases/Report/Commands/Create/CreateReportCommandResponse.cs
+++ src/VizinhoDAgua.Application/UseCases/Report/Commands/Create/CreateReportCommandResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VizinhoDAgua.Application.UseCases.Report.Commands.Create
+﻿namespace VizinhoDAgua.Application.UseCases.Report.Commands.Create
 {
     public class CreateReportCommandResponse
     {
--- src/VizinhoDAgua.Domain/Dtos/CepDto.cs
+++ src/VizinhoDAgua.Domain/Dtos/CepDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VizinhoDAgua.Application.Dtos
+﻿namespace VizinhoDAgua.Domain.Dtos
 {
     public sealed record CepResponseDto(
         string? Road,
