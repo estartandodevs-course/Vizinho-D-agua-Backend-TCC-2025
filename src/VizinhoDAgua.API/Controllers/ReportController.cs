039ed1f8e93c76730a9bd6817778aa45c360e78a
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-25 17:06:35 -0300

    refactor: integrate ReportController with BaseController
    - Substituir a implementação personalizada do ReportController por um BaseController genérico
    - Remover ações manuais e delegar o fluxo CRUD à abstração da classe base
    - Melhorar a consistência entre os controladores e reduzir a lógica duplicada

--- src/VizinhoDAgua.API/Controllers/ReportController.cs
+++ src/VizinhoDAgua.API/Controllers/ReportController.cs
@@ -1,44 +1,26 @@
-﻿using Amazon.Runtime.Internal;
+﻿using AutoMapper;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry.Trace;
+using VizinhoDAgua.API.Controllers.Abstractions;
 using VizinhoDAgua.Application.Dtos;
 using VizinhoDAgua.Application.UseCases.Report.Commands.Create;
-using VizinhoDAgua.Application.UseCases.User.Commands.Create;
+using VizinhoDAgua.Application.UseCases.Report.Commands.Delete;
+using VizinhoDAgua.Application.UseCases.Report.Commands.Update;
+using VizinhoDAgua.Application.UseCases.Report.Queries.GetAll;
+using VizinhoDAgua.Application.UseCases.Report.Queries.GetById;
 using VizinhoDAgua.Domain.Entities;
 
 namespace VizinhoDAgua.API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ReportController: ControllerBase
+    public class ReportController: BaseController
+        <
+            ReportEntity,  CreateReportRequest, CreateReportCommand, CreateReportCommandResponse,
+            GetReportByIdQuery, GetReportByIdQueryResponse, GetAllReportsQuery, GetAllReportsQueryResponse,
+            UpdateReportRequest, UpdateReportCommand, DeleteReportCommand
+        >
     {
-        private readonly IMediator _mediator;
-
-        public ReportController(IMediator mediator)
+        public ReportController(IMediator mediator, IMapper mapper) 
+            : base(mediator, mapper)
         {
-            _mediator = mediator;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(
-            [FromBody] CreateReportRequest request)
-        {
-            var command = new CreateReportCommand(
-                request.Description,
-                request.ReportType,
-                request.ReporterId,
-                request.PostalCode,
-                request.StateCode,
-                request.City,
-                request.Neighborhood,
-                request.Road,
-                request.Lat,
-                request.Lon
-            );
-
-            var response = await _mediator.Send(command); // manda o comando pro handler
-            return StatusCode((int)response.StatusCode, response); // retorna 201 (Created) e a resposta
         }
     }
 }
