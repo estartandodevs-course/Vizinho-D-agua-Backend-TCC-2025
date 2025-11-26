32130df7262955396a02cd0d73dda1ef46cc74fc
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-26 09:32:57 -0300

    feat: add AlertRepository, interface and DI configuration
    - Criação da interface IAlertRepository no domínio.
    - Implementação do AlertRepository herdando de Repository<AlertEntity>.
    - Registro do repositório no container de DI.

---
+++ src/VizinhoDAgua.Domain/Repositories/IAlertRepository.cs
@@ -0,0 +1,10 @@
+using VizinhoDAgua.Domain.Entities;
+using VizinhoDAgua.Domain.Repositories.Abstractions;
+
+namespace VizinhoDAgua.Domain.Repositories
+{
+    public interface IAlertRepository : IRepository<AlertEntity>
+    {
+    }
+}
+
