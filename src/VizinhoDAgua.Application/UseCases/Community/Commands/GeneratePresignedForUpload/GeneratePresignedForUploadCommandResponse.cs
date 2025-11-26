be53a9e58c22e08672d4fcf6d9c82eaa5d6aee9b
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-26 00:17:34 -0300

    feat: added generatePresignedForUpload in community Crud

---
+++ src/VizinhoDAgua.Application/UseCases/Community/Commands/GeneratePresignedForUpload/GeneratePresignedForUploadCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace VizinhoDAgua.Application.UseCases.Community.Commands.GeneratePresignedForUpload
+{
+    public class GeneratePresignedForUploadCommandResponse
+    {
+        public string PresignedUrl { get; private set; }
+
+        public GeneratePresignedForUploadCommandResponse(string presignedUrl)
+        {
+            PresignedUrl = presignedUrl;
+        }
+    }
+}
