1467b14c14b67a9d9d64eb2f6f9e7a429faa10c9
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-26 13:03:09 -0300

    remove: remove ICepService

--- src/VizinhoDAgua.Application/Interfaces/ICepService.cs
+++
@@ -1,9 +0,0 @@
-﻿using VizinhoDAgua.Domain.Dtos;
-
-namespace VizinhoDAgua.Application.Interfaces
-{
-    public interface ICepService
-    {
-        Task<CepResponseDto?> GetAddressByCepAsync(string cep, CancellationToken cancellationToken =  default);
-    }
-}
