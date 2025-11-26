760553c8989a37e6be144f277720636d3605eece
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-26 09:31:40 -0300

    chore: adjustments and standardization in enums

--- src/VizinhoDAgua.Domain/Entities/Enum/AlertStatus.cs
+++ src/VizinhoDAgua.Domain/Entities/Enum/AlertStatus.cs
@@ -2,8 +2,8 @@ namespace VizinhoDAgua.Domain.Entities.Enum
 {
     public enum AlertStatus
     {
-        UnderVerification = 0,
-        Reviewed = 1,
-        Official = 2
+        UnderVerification,
+        Reviewed,
+        Official
     }
 }
