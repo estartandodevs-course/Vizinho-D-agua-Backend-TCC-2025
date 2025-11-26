c83e6405dd5d958afe073ae7225049313d53f093
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-26 09:32:09 -0300

    chore: remove unused usings from Alert files

--- src/VizinhoDAgua.Domain/Repositories/IReportRepository.cs
+++ src/VizinhoDAgua.Domain/Repositories/IReportRepository.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VizinhoDAgua.Domain.Entities;
+﻿using VizinhoDAgua.Domain.Entities;
 using VizinhoDAgua.Domain.Repositories.Abstractions;
 
 namespace VizinhoDAgua.Domain.Repositories
 {
     public interface IReportRepository : IRepository<ReportEntity>
     {
-
     }
 }
