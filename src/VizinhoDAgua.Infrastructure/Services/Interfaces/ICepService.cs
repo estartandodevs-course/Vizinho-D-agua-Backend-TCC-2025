fe494065224ea0ccf333a7673343eea8e0eacac6
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-26 13:01:16 -0300

    refactor: add this service and interface in infrastructure

---
+++ src/VizinhoDAgua.Infrastructure/Services/Interfaces/ICepService.cs
@@ -0,0 +1,9 @@
+﻿using VizinhoDAgua.Domain.Dtos;
+
+namespace VizinhoDAgua.Infrastructure.Services.Interfaces
+{
+    public interface ICepService
+    {
+        Task<CepResponseDto?> GetAddressByCepAsync(string cep, CancellationToken cancellationToken =  default);
+    }
+}
