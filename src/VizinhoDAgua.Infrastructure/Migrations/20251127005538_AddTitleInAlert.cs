4b8c723496740f779730250da496b77cfd9b0295
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-26 21:58:03 -0300

    feat: add the AddTitleInAlert migration

---
+++ src/VizinhoDAgua.Infrastructure/Migrations/20251127005538_AddTitleInAlert.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VizinhoDAgua.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleInAlert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "AlertEntity",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "AlertEntity");
+        }
+    }
+}
