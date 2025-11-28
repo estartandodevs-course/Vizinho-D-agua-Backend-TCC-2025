ce7a3daabdeb7bed412780d7993ebb6991be9c8d
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-11-28 20:42:26 -0300

    feat: edit DTOs and configure AutoMapper
    - Adicionados campos Title e Description nos DTOs de Alert.
    - Criado UpdateAlertRequest com todos os campos possíveis.
    - Atualizados os mapeamentos do AutoMapper para suportar UpdateAlertCommand com todos os campos.

--- src/VizinhoDAgua.Application/Dtos/AlertDto.cs
+++ src/VizinhoDAgua.Application/Dtos/AlertDto.cs
@@ -1,5 +1,10 @@
+using VizinhoDAgua.Domain.Entities.Enum;
+
 namespace VizinhoDAgua.Application.Dtos
 {
-    public record CreateAlertRequest(string Description, string PostalCode, string? City, string? StateCode, string? Road, string? Neighborhood);
-    public record UpdateAlertStatusRequest(int Status);
+    public record CreateAlertRequest(string Title, string Description, string PostalCode, string? City, 
+        string? StateCode, string? Road, string? Neighborhood);
+    
+    public record UpdateAlertRequest(AlertStatus? Status, string? Title, string? Description, string? PostalCode, 
+        string? City, string? StateCode, string? Road, string? Neighborhood);
 }
