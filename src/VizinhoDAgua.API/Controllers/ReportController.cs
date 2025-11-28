6ab662ef750d7249ccbcb8a0a6ead7cd1855e2f8
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2025-11-28 16:31:11 -0300

    feat: added attachments implementations

--- src/VizinhoDAgua.API/Controllers/ReportController.cs
+++ src/VizinhoDAgua.API/Controllers/ReportController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using VizinhoDAgua.API.Controllers.Abstractions;
 using VizinhoDAgua.Application.Dtos;
 using VizinhoDAgua.Application.UseCases.Report.Commands.Create;
 using VizinhoDAgua.Application.UseCases.Report.Commands.Delete;
+using VizinhoDAgua.Application.UseCases.Report.Commands.GeneratePresignedForUpload;
 using VizinhoDAgua.Application.UseCases.Report.Commands.Update;
 using VizinhoDAgua.Application.UseCases.Report.Queries.GetAll;
 using VizinhoDAgua.Application.UseCases.Report.Queries.GetById;
@@ -21,6 +23,19 @@ namespace VizinhoDAgua.API.Controllers
         public ReportController(IMediator mediator, IMapper mapper) 
             : base(mediator, mapper)
         {
+        }
+
+        [HttpPost("{id}/upload")]
+        public async Task<IActionResult> GeneratePresignedUrlForUpload(
+            [FromRoute] Guid id,
+            [FromBody] GeneratePresignedUrlDto request
+        )
+        {
+            var command = new GeneratePresignedForUploadAttachmentCommand(id, request.FileName);
+
+            var response = await _mediator.Send(command);
+            
+            return StatusCode((int)response.StatusCode, response);
         }
     }
 }
---
+++ src/VizinhoDAgua.Application/UseCases/Report/Commands/GeneratePresignedForUpload/GeneratePresignedForUploadAttachmentCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace VizinhoDAgua.Application.UseCases.Report.Commands.GeneratePresignedForUpload
+{
+    public class GeneratePresignedForUploadAttachmentCommandResponse
+    {
+        public string PresignedUrl { get; private set; }
+
+        public GeneratePresignedForUploadAttachmentCommandResponse(string presignedUrl)
+        {
+            PresignedUrl = presignedUrl;
+        }
+    }
+}
